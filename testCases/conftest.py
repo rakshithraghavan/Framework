b0879683f10e8bfa81527619a36cb3a7771446a8
rakshithraghavan <rakshithraghavan09@gmail.com>
2023-03-06 16:52:22 +0530

    browser_name

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -1,6 +1,33 @@
 from selenium import webdriver
 import pytest
 
+import time
+driver = None
+
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_name", action="store", default="chrome"
+    )
+
+
+@pytest.fixture(scope="class")      ## decorator to create fixture and can be used on class level
+def setup(request):     ## request is predefined object, which will be auto populated by pytest
+    global driver
+    browser_name=request.config.getoption("browser_name")   #pytest --browser_name "firefox"
+
+     # parameter which can be passed with pytest command
+    if browser_name == "chrome":
+        driver = webdriver.Chrome("C:/Program Files/chromedriver.exe")
+    elif browser_name == "firefox":
+        driver = webdriver.Firefox()
+    elif browser_name == "IE":
+        print("IE driver")
+    else:
+        driver = webdriver.Chrome()
+
+
 @pytest.fixture()
 def setup():
     driver=webdriver.Chrome("C:/Program Files/chromedriver.exe")
