f5ebeaf64b65c253636d1727b5946925b2cf6f6f
rakshithraghavan <rakshithraghavan09@gmail.com>
2023-03-07 18:49:15 +0530

    automation.log

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -1,24 +1,35 @@
 import pytest
 from selenium import webdriver
 from pageObjects.LoginPage import LoginPage
+from utilities.readProperties import ReadConfig
+from utilities.customLogger import LogGen
 #pytest -v -s
 
 class Test_001_Login:
-    baseURL="https://admin-demo.nopcommerce.com/login?ReturnUrl=%2Fadmin%2F"
-    username="admin@yourstore.com"
-    password="admin"
+    baseURL=ReadConfig.getApplicationURL()
+    username=ReadConfig.getUsermail()
+    password=ReadConfig.getPassword()
 
-    def test_homepageTitle(self,setup):
+    logger=LogGen.loggen()
+
+    def test_homePageTitle(self,setup):
+        self.logger.info("**********Test_001_Login**********")
+        self.logger.info("**********Verifying Home Page Title**********")
         self.driver=setup
         self.driver.get(self.baseURL)
         act_title=self.driver.title
-        self.driver.close()
         if act_title=="Your store. Login":
             assert True
+            self.driver.close()
+            self.logger.info("**********Home Page Title Test Passed**********")
         else:
+            self.driver.save_screenshot(".\\Screenshots\\"+"test_homePageTitle.png")
+            self.driver.close()
+            self.logger.error("**********Home Page Title Test Failed**********")
             assert False
 
     def test_login(self, setup):
+        self.logger.info("**********Verifying Login Test**********")
         self.driver = setup
         self.driver.get(self.baseURL)
         self.lp=LoginPage(self.driver)
@@ -29,8 +40,10 @@ class Test_001_Login:
 
         if act_title=="Dashboard / nopCommerce administration":
             assert True
+            self.logger.info("**********Login Test Passed**********")
             self.driver.close()
         else:
             self.driver.save_screenshot(".\\Screenshots\\"+"test_homePageTitle.png")
             self.driver.close()
+            self.logger.error("**********Login Test Failed**********")
             assert False
---
+++ utilities/readProperties.py
@@ -0,0 +1,21 @@
+import configparser
+
+config=configparser.RawConfigParser()
+config.read(".\\Configurations\\config.ini")
+
+
+class ReadConfig:
+    @staticmethod
+    def getApplicationURL():
+        url=config.get('common info', 'baseURL')
+        return url
+
+    @staticmethod
+    def getUsermail():
+        username=config.get('common info', 'usermail')
+        return username
+
+    @staticmethod
+    def getPassword():
+        password=config.get('common info', 'password')
+        return password
